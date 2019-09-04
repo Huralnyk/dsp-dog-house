c5c56c7eeae9ffbb434bbedc661ebcf6ac0c165c
Oleksii Huralnyk <alexey.huralnyk@gmail.com>
2019-09-04 18:01:31 +0300

    initial commit

---
+++ Pitch Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pitch Detector
+//
+//  Created by Oleksii Huralnyk on 9/4/19.
+//  Copyright © 2019 Hobot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pitch DetectorTests/Pitch_DetectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pitch_DetectorTests.swift
+//  Pitch DetectorTests
+//
+//  Created by Oleksii Huralnyk on 9/4/19.
+//  Copyright © 2019 Hobot. All rights reserved.
+//
+
+import XCTest
+@testable import Pitch_Detector
+
+class Pitch_DetectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
